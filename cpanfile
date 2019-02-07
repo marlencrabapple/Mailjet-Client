d47d8c3489e3c8868914b09d1d83f61aaf239dc7
Ian P Bradley <ian.bradley@studiocrabapple.com>
2019-02-07 15:17:53 -0800

    Add super basic support for send 3.1 and a couple of other informational endpoints

--- cpanfile
+++ cpanfile
@@ -4,6 +4,8 @@ requires 'JSON::MaybeXS';
 requires 'LWP::UserAgent';
 requires 'LWP::Protocol::https';
 requires 'HTTP::Request::Common';
+requires 'Syntax::Keyword::Try';
+requires 'URI';
 
 on test => sub {
     requires 'Test::More', '0.96';
--- lib/Mailjet/Client.pm
+++ lib/Mailjet/Client.pm
@@ -5,10 +5,12 @@ use v5.28;
 use strict;
 use warnings;
 
+use URI;
 use JSON::MaybeXS;
 use Carp qw(croak);
 use LWP::UserAgent;
-use HTTP::Request::Common qw(GET POST DELETE);
+use Syntax::Keyword::Try;
+use HTTP::Request::Common qw(GET POST);
 
 our $VERSION = '0.01';
 
@@ -16,39 +18,120 @@ our $base = 'https://api.mailjet.com/v3';
 our $ua = LWP::UserAgent->new;
 
 sub new {
-  my $class = shift;
-  my %args = scalar @_ == 1 ? @_->%* : @_;
+  my ($class, %args) = @_;
 
-  croak "Missing public API key" unless $args{api_key};
-  croak "Missing private API key" unless $args{secret_key};
+  croak 'Missing public API key.' unless $args{api_key};
+  croak 'Missing secret API key.' unless $args{secret_key};
 
-  my %attribs = %args{qw(api_key secret_key)};
+  my %attribs = %args{qw(api_key secret_key version)};
+  $attribs{sandbox_mode} = 1 if $args{sandbox_mode};
 
   return bless \%attribs, $class
 }
 
 sub send_mail {
-  my ($self, $body) = @_;
-  $body = encode_json($body);
+  my ($self, %args) = @_;
 
-  my $req = POST "$base/send";
-  $req->content($body);
+  my $version = $args{version} ? $args{version} : $$self{version};
+  $args{json_body} = 1;
+
+  return $version && $version == 3? $self->send_mail_3(%args) : $self->send_mail_31(%args)
+}
+
+sub send_mail_31 {
+  my ($self, %args) = @_;
+
+  $args{data}->{SandboxMode} = 1
+    if $$self{sandbox_mode} || $args{data}->{SandboxMode};
+
+  return $self->post("$base.1/send", %args)
+}
+
+sub send_mail_3 {
+  return shift->post("$base/send", @_)
+}
+
+sub get_message {
+  my ($self, $id, %args) = @_;
+
+  if($args{no_id} && !$id) {
+    $id = ''
+  }
+  elsif(!$id) {
+    croak 'Missing message ID.'
+  }
+
+  my $type = $args{type} ? $args{type} : '';
+
+  return $self->get("$base/REST/message$type/$id", %args)
+}
+
+sub get_messages {
+  my ($self, %args) = @_;
+  return $self->get_message(undef, %args, no_id => 1)
+}
+
+sub get_message_history {
+  return shift->get_message(@_, type => 'history')
+}
+
+sub get_message_info {
+  return shift->get_message(@_, type => 'information')
+}
+
+sub get_messages_info {
+  return shift->get_message_info(undef, @_, no_id => 1) 
+}
+
+sub get {
+  my ($self, $uri_str, %args) = @_;
+
+  my $uri = URI->new($uri_str);
+  $uri->query_form($args{data}) if scalar keys $args{data}->%*;
+
+  return $self->send_request(GET $uri->as_string)
+}
+
+sub post {
+  my ($self, $uri_str, %args) = @_;
+  my $req;
+
+  if($args{json_body}) {
+    $req = POST $uri_str;
+    $req->content(encode_json($args{data}))
+  }
+  else {
+    $req = POST $uri_str, $args{data}
+  }
 
   return $self->send_request($req)
 }
 
 sub send_request {
-  my ($self, $req) = @_;
+  my ($self, $req, %args) = @_;
   
-  $req->authorization_basic($$self{api_key}, $$self{secret_key});
+  $req->authorization_basic($self->@{qw(api_key secret_key)});
   $req->header('content-type' => 'application/json');
 
   my $res = $ua->request($req);
 
   return decode_json($res->decoded_content)
-    if($res->is_success);
-    
-  croak $res->status_line, $res->decoded_content;
+    if $res->is_success;
+
+  my $error_content;
+
+  try {
+    $error_content = decode_json($res->decoded_content)
+  }
+  catch {
+    croak $res->status_line, $res->decoded_content
+  }
+
+  return {
+    code => $res->code,
+    message => $res->message,
+    content => $error_content
+  }
 }
 
 1
